90438e9cdac337d7680d9a5f8593002c7cafa171
pooja <pooja@MYOWNDELL>
2025-05-06 13:47:55 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            string apiUrl = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=b983a0352528e1772dbb455347ab7a0c"; // Replace with actual API URL
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+
+                string data = await response.Content.ReadAsStringAsync();
+                return View(data, "application/json"); // Returning JSON response
+            }
+            catch (HttpRequestException e)
+            {
+                return BadRequest($"Error: {e.Message}");
+            }
+
+            //return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
